7a971d37b8a79dc8362d6d22b6ba17925ce1dce0
tester <alekseyOT325@yandex.ru>
2023-09-26 21:15:42 +0300

    Upgrade

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
         int price = 10_000;
-        int needMin = 20;
-        int bonus = price / needMin;
-        String message = "Начислено бонусных миль: ";
-        String message2 = "Бонусные мили не начислены";
-
-        if (bonus > 0) {
-            System.out.println(message + bonus);
-        } else {
-            System.out.println(message2);
-        }
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
